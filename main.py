7d10bbe759c2758e92d5d41f35b00ad3b94490f7
Archinme <brokenmopflag@gmail.com>
2025-08-25 23:22:12 +0100

    Added save generated content functionality to file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
-from ollama_model import ollama_chat
+
 from gemini_model import gemini_chat
 from tts import speakText
 
+
 def main_func(input_content):
     model_content = ollama_chat(input_content)["message"]["content"]
-    # model_content = gemini_chat(input_content)
+    # model_content = gemini_chat(input_content).text
     model_answer = (model_content)
-    print(model_answer)
+    
+    with open('model_answer.txt', 'w') as f:
+        f.write(model_answer)
 
     speakText(model_content)
 
